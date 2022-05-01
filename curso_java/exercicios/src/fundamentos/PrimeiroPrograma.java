dd7d6a23275c4eaae2a7742d7a4d4349e9e38cfa
Kevyn Gorito <kevy4job@gmail.com>
2022-05-01 17:23:15 -0300

    Entendo sobre blocos de código

--- curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,8 @@ package fundamentos;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		System.out.println("Primeiro programa parte #01");
+		System.out.println("Primeiro programa parte #02");
+
 	}
 }
