c093e12e835204dd972010407b590964818de577
Kevyn Gorito <kevy4job@gmail.com>
2022-05-01 17:42:04 -0300

    Explorando a criação de classes e packges e entendendo de uma maneira macro como funciona a colaboração entre classes para o funcionamento de uma aplicação em java

--- curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -5,6 +5,5 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa parte #01");
 		System.out.println("Primeiro programa parte #02");
-
 	}
 }
