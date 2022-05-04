d90a1d23643783782ab83b9c63d1df1bcbbb2a64
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-04 16:57:31 -0300

    Reatribuindo as variáveis para utilização das mesmas

--- curso_java/exercicios/src/fundamentos/Temperatura.java
+++ curso_java/exercicios/src/fundamentos/Temperatura.java
@@ -4,11 +4,17 @@ public class Temperatura {
 
 	public static void main(String[] args) {
 		//Fórmula farenhairt (°F - 32) * 5/9 = °C
+		//CTRL + ALT + SETA (V) duplica uma linha para mover ALT + ^
 		
-		double temp1 = 35;
+		double temp1 = 86;
 		final double FORMULATEMP1 = 32;
 		final double FORMULACONVERT = 5.0 / 9.0;
 		double resultC = (temp1 - FORMULATEMP1) * FORMULACONVERT;
-		System.out.println(resultC);
+		
+		System.out.println("Exemplo 1: O resultado é " + resultC + "°C. \n");		
+		temp1 = 150;
+		resultC = (temp1 - FORMULATEMP1) * FORMULACONVERT;
+		System.out.println("Exemplo 2: O resultado é " + resultC + "°C.");
+
 	}
 }
