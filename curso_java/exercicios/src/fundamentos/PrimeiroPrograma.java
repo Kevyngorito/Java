5e7292dc7a3f85d137c7de84d78c34e973027790
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-04 11:00:07 -0300

    Entendendo a importância de realizar comentários de forma coerente para com a clareza do código

--- curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ curso_java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,14 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 
+	/**
+	 * @param args
+	 * @author kevyn.gorito
+	 * @since JDK1.0
+	 * Este tipo de comentário relata informações no javadocs
+	 */
+	
+	// Uma sentença de código termina com ; Para comentar CTRL + /
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa parte #01");
 		System.out.println("Primeiro programa parte #02");
