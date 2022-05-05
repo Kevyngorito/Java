ecb379527b147ef4f7cd8b6c034dfc6b22fea39c
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-05 15:24:26 -0300

    Introdução a atribuição por inferência

---
+++ curso_java/exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) { //porta de entrada para a execução de um código
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		//Atribui o tipo da variável por inferência (pelo valor setado);
+		var b = 7.5; 
+		System.out.println(b);
+	}
+}
