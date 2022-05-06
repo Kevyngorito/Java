8043897f42412166c11ff68c5713ea8a24b53dfa
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-06 11:48:07 -0300

    Realizando um exercício básico sobre a notação ponto e seus métodos

---
+++ curso_java/exercicios/src/fundamentos/ExercicioNotacaoPonto.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class ExercicioNotacaoPonto {
+
+	public static void main(String[] args) {
+		//Validando por atribuição um a um;
+		String x = "Bem vindo X";
+		x = x.replaceAll("X", "senhor");
+		x = x.toUpperCase();
+		System.out.println(x);
+		
+		//Validando direto na saída
+		System.out.println("bom dia menino".replace("menino", "rapaz").toUpperCase());
+		
+		//Quebrando notação ponto
+		System.out.println("Olá como vai você"
+				.replace("você", "?")
+				.concat("Você está bem?")
+				.toUpperCase());
+	}
+}
