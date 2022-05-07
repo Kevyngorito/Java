026bc5f3241ebd88471bc87e633d130061d5ba0e
Kevyn Gorito <kevy4job@gmail.com>
2022-05-07 15:25:18 -0300

    Projeto IMC finalizado

--- curso_java/exercicios/src/personalChallengers/Imc.java
+++ curso_java/exercicios/src/personalChallengers/Imc.java
@@ -5,25 +5,38 @@ import java.util.Scanner;
 public class Imc {
 
 	public static void main(String[] args) {
-		//entry mode
-		Scanner entrada = new Scanner(System.in);
-		
-		//First task
+		// entry mode
+		Scanner entry = new Scanner(System.in);
+
+		// First task
 		System.out.println("Say your name: ");
-		String name = entrada.nextLine();
-		
-		//Second task
+		String name = entry.nextLine();
+
+		// Second task
 		System.out.println("Say your weigth: ");
-		double weigth = entrada.nextFloat();
-		
-		//third task
+		double weigth = entry.nextDouble();
+
+		// third task
 		System.out.println("Say your height: ");
-		double heigth = entrada.nextDouble();
+		double heigth = entry.nextDouble();
+
+		double imc;
+		imc = (weigth / (heigth * heigth));
 		
-		double imc = (weigth / (heigth * heigth));
-		
-		System.out.printf("Olá %s Seu IMC é %f", name, imc);
-			
-		entrada.close();
+
+		if (imc < 18.5) {
+			System.out.printf("Olá %s Seu IMC é %f e está abaixo do peso normal", name, imc);
+		} else if (imc > 18.4 && imc < 25) {
+			System.out.printf("Olá %s Seu IMC é %f peso normal", name, imc);
+		} else if (imc > 25 && imc < 30) {
+			System.out.printf("Olá %s Seu IMC é %f excesso de peso", name, imc);
+		} else if (imc > 30 && imc < 35) {
+			System.out.printf("Olá %s Seu IMC é %f obesidade classe I", name, imc);
+		} else if (imc > 35 && imc < 40) {
+			System.out.printf("Olá %s Seu IMC é %f obesidade classe II", name, imc);
+		} else if (imc >= 40) {
+			System.out.printf("Olá %s Seu IMC é %f obesidade classe III", name, imc);
+		}
+		entry.close();
 	}
 }
