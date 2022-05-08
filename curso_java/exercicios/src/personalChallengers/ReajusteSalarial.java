a98049f9398959966baa1c8da2261a180c48ae96
Kevyn Gorito <kevy4job@gmail.com>
2022-05-07 23:08:58 -0300

    Desafio pessoal de reajuste salarial em andamento

---
+++ curso_java/exercicios/src/personalChallengers/ReajusteSalarial.java
@@ -0,0 +1,31 @@
+package personalChallengers;
+
+import java.util.Scanner;
+
+public class ReajusteSalarial {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Informe o seu nome: ");
+		String nome = scan.nextLine();
+		System.out.print("Informe o seu sobrenome: ");
+		String sobrenome = scan.nextLine();
+		System.out.print("Informe o seu salario: ");
+		String salario = scan.nextLine().replace(",", ".");
+		
+		double salario1 = Double.parseDouble(salario);
+		double salMedio;
+		
+		if (salario1 < 1500) {
+			salMedio = salario1 * (10 / 100); 
+			System.out.printf(" %s %s Seu salário %f foi reajustado em 10%. Seu novo salário é %f", nome, sobrenome ,salario1, salMedio);
+		} else if (salario1 > 2000) {
+			salMedio = salario1 * (5 / 100); 
+			System.out.printf(" %s %s Seu salário %f foi reajustado em 5%. Seu novo salário é %f", nome, sobrenome ,salario1, salMedio);
+		}
+		
+		
+		scan.close();
+	}
+}
