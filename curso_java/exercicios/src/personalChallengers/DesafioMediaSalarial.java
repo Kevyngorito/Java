41f8e83cd4f868393dcc7fd09360a50a5e2dd860
Kevyn Gorito <kevy4job@gmail.com>
2022-05-07 22:25:09 -0300

    Resolvendo desafio de código Média salarial

---
+++ curso_java/exercicios/src/personalChallengers/DesafioMediaSalarial.java
@@ -0,0 +1,27 @@
+package personalChallengers;
+
+import java.util.Scanner;
+
+public class DesafioMediaSalarial {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro salário: ");
+		String salario1 = teclado.nextLine().replace(",", ".");
+		System.out.println("Informe o segundo salário: ");
+		String salario2 = teclado.nextLine().replace(",", ".");
+		System.out.println("Informe o terceiro salário: ");
+		String salario3 = teclado.nextLine().replace(",", ".");
+		
+		double s1 = Double.parseDouble(salario1);
+		double s2 = Double.parseDouble(salario2);
+		double s3 = Double.parseDouble(salario3);
+		
+		double media = ((s1 + s2 + s3) / 3);
+		
+		System.out.println("A média salarial do último trimestre foi: " + media);
+		
+		teclado.close();
+	}
+}
