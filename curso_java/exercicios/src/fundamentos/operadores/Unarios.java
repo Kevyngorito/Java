b27bad0d884a76cb03371b88a75961d0553d8c37
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-09 15:16:36 -0300

    Operadores unários com formas pré fixadas e pós fixadas

---
+++ curso_java/exercicios/src/fundamentos/operadores/Unarios.java
@@ -0,0 +1,34 @@
+package fundamentos.operadores;
+
+public class Unarios {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		// FORMA PÓS FIXADA
+		//incremento
+		a++; //a = a + 1;
+		//decremento
+		a--; //a = a - 1;
+		
+		
+		// FORMA PRÉ FIXADA
+		
+		++b; // b = b + 1;
+		--b; // b = b - 1;
+		
+		System.out.println(a);
+		System.out.println(b);
+		/* Por precedência a forma pré fixada tem maior precedência do que comparação ou pós fixada
+		 ou seja, no exemplo abaixo ele primeiro incrementa o a, depois compara a com b, retornando true
+		 e depois decrementa o b
+		 */
+		System.out.println("Mini Desafio...");
+		System.out.println(++a == b--);
+		System.out.println(a == b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
