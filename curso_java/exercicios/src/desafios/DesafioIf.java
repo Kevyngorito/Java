e7c720ade1a8410fe024e2f6bcfad2027bc6b336
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-13 14:00:45 -0300

    Resolvendo primeiro desafio de código, byPassando a condição IF

---
+++ curso_java/exercicios/src/desafios/DesafioIf.java
@@ -0,0 +1,16 @@
+package desafios;
+
+public class DesafioIf {
+
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		/*Descobrir o porque exibe as mensagens mesmo não atendendo ao requisito da condição
+		 isso se dá por ter o ; depois da condição
+		 */
+		if(nota >= 9.0); {
+			System.out.println("Quadro de honra!");
+			System.out.println("Você é fera!");
+		}
+	}
+}
