a301f2a7f0313a9fbe297f2440650f74d9c07370
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-16 15:11:04 -0300

    Laço for decrescente

--- curso_java/exercicios/src/desafios/DesafioWhile.java
+++ curso_java/exercicios/src/desafios/DesafioWhile.java
@@ -9,25 +9,20 @@ public class DesafioWhile {
 		Scanner entrada = new Scanner(System.in);
 			
 		double nota = 0;
-		int count = 0;
-		double total = 0;
-		double media = 0;
-		
+		int    count = 0;
+		double total = 0;	
 		while(nota >= 0 && nota <= 10) {
-
 			total += nota;
 			System.out.println("Informe uma nota: ");
 			nota = entrada.nextDouble();
 			count ++;
-			
 				if (nota < 0 || nota > 10) {
 					count --;
 				}
-			
-			media = total / count;
-
 		}
-		System.out.printf("Foram digitadas %d notas válidas, totalizando %.2f com uma média de %.2f ", count, total, media);
+		
+			double media = total / count;
+			System.out.printf("Foram digitadas %d notas válidas, totalizando %.2f com uma média de %.2f ", count, total, media);
 					
 		entrada.close();
 
