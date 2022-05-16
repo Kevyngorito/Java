8023f89a71cc6e4cccc9d11ffed71e293fa5237e
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-16 14:04:45 -0300

    Resolvendo desafio de código com While

---
+++ curso_java/exercicios/src/desafios/DesafioWhile.java
@@ -0,0 +1,35 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+			
+		double nota = 0;
+		int count = 0;
+		double total = 0;
+		double media = 0;
+		
+		while(nota >= 0 && nota <= 10) {
+
+			total += nota;
+			System.out.println("Informe uma nota: ");
+			nota = entrada.nextDouble();
+			count ++;
+			
+				if (nota < 0 || nota > 10) {
+					count --;
+				}
+			
+			media = total / count;
+
+		}
+		System.out.printf("Foram digitadas %d notas válidas, totalizando %.2f com uma média de %.2f ", count, total, media);
+					
+		entrada.close();
+
+	}
+}
