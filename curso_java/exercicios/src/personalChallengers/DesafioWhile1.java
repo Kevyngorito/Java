aec73ec64191092fe2353e211e32e55eb1585b19
Kevyn Gorito <kevyndccap@gmail.com>
2022-05-16 10:35:39 -0300

    Resolvendo desafio pessoa de while utilizando contains e negação !

---
+++ curso_java/exercicios/src/personalChallengers/DesafioWhile1.java
@@ -0,0 +1,20 @@
+package personalChallengers;
+
+import java.util.Scanner;
+
+public class DesafioWhile1 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String input = "";
+		
+		while (!input.contains("#")) {
+			System.out.println("Bom dia, por favor informe seu ID para liberar a entrada: ");
+
+			input =  entrada.next();
+		}
+		
+		
+		entrada.close();
+	}	
+}
