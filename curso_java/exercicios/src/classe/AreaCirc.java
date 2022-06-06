66ff6a9964b3ee43c76ae72fe65c6944632b3b45
Kevyn Gorito <kevyndccap@gmail.com>
2022-06-06 10:27:14 -0300

    Estudando sobre membros de classe vs instância

---
+++ curso_java/exercicios/src/classe/AreaCirc.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class AreaCirc {
+
+	double raio;
+	//final cria uma constante static atribui a variável como sendo da classe e não do objeto
+	final static double PI = 3.1415;
+	
+	public AreaCirc(double raioInicial) {
+		raio = raioInicial;
+	}
+	
+	double area() {
+		return PI * Math.pow(raio, 2);
+	}
+	
+	static double area(double raio) {
+		return PI * Math.pow(raio, 2);
+	}
+}
