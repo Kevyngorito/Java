1172a4aa3de07c79596e366cf4e41db6879a891e
Kevyn Gorito <kevy4job@gmail.com>
2022-06-22 05:27:15 -0300

    Add someone classes about car, like what onCar, CheckonCar and change the status to variable on for static false

--- curso_java/exercicios/src/personalChallengers/Car.java
+++ curso_java/exercicios/src/personalChallengers/Car.java
@@ -5,19 +5,37 @@ public class Car {
 	String name;
 	String model;
 	int year;
-	boolean on;
+	boolean on = false;
+	int speed;
 	
-	Car(String name, String model, int year, boolean on){
+	Car(String name, String model, int year, int speed){
 		this.name = name;
 		this.model = model;
 		this.year = year;
-		this.on = on;
+		this.speed = speed;
 	}
 	
-	String onCar () {
-		if(on) {
-		return	"The car is on";
-		} 
-		return "The car is off";
+	public String showCar() {
+		return  "The " + this.name + " car " + "year " + this.year + " is the " + this.speed + " Km\\h.";
 	}
+	
+	String checkOnCar() {
+		if(!on) {
+		return	"The car is off";
+		} 
+		this.on = true;
+		return "The car is on vrum vrum...";
+	}
+	
+	void onCar() {
+		if(!on) {
+		this.on = true;
+		System.out.println("Vrum vrum the car is on!");
+		}
+	}
+	
+	public int speedUp() {
+		return this.speed += 30;
+	}
+	
 }
--- curso_java/exercicios/src/personalChallengers/ManagerCar.java
+++ curso_java/exercicios/src/personalChallengers/ManagerCar.java
@@ -4,8 +4,15 @@ public class ManagerCar {
 
 	public static void main(String[] args) {
 		
-		Car c1 = new Car("Compass", "Jeep", 2020, false);
+		Car c1 = new Car("Compass", "Jeep", 2020,0);
+		System.out.println("Is the car on? ");
+		System.out.println(c1.checkOnCar());
+		c1.onCar();
 		System.out.println(c1.on);
-		System.out.println(c1.onCar());
+		c1.onCar();
+		System.out.println(c1.checkOnCar());
+		System.out.println(c1.speedUp());
+		System.out.println(c1.speedUp());
+		System.out.println(c1.showCar());
 	}
 }
