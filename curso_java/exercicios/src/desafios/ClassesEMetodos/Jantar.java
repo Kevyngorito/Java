57431ff42ec90cdf0c3f6f0cd38109a3de354c37
Kevyn Gorito <kevy4job@gmail.com>
2022-06-22 04:51:12 -0300

    first job 22/06/22

--- curso_java/exercicios/src/desafios/ClassesEMetodos/Jantar.java
+++ curso_java/exercicios/src/desafios/ClassesEMetodos/Jantar.java
@@ -12,16 +12,15 @@ public class Jantar {
 		Pessoa p1 = new Pessoa("Julio", 85.0); 
 		Pessoa p2 = new Pessoa("Amanda", 76.0);
 		
-		System.out.println("Peso antes da refeição: " + p1.showResults());
+		System.out.println(p1.showResults() + "[Peso antes da refeição]");
 		p1.eating(c3);
 		p1.eating(c2);
 		p1.eating(c1);
-		System.out.println("Fiz a refeição completa (arroz, feijão e frango), após o jantar o meu peso é " + p1.weight);
-		
-		System.out.println("Peso antes da refeição: " + p2.showResults());
+		System.out.println("Fiz a refeição completa (arroz, feijão e frango), após o jantar o meu peso é " + p1.weight + "Kgs\n");
+		System.out.println(p2.showResults() + "[Peso antes da refeição]");
 		p2.eating(c3);
 		p2.eating(c1);
-		System.out.println("Não fiz a refeição completa comi apenas (arroz e frango). Após a refeição o meu peso é " + p2.weight);
+		System.out.println("Não fiz a refeição completa, comi apenas (arroz e frango). Após a refeição o meu peso é " + p2.weight);
 
 	}
 
