e4ab19751b6c1a07577dc146c6b7bfe435c5dd88
Kevyn Gorito <kevy4job@gmail.com>
2022-06-30 22:44:04 -0300

    Reestruturando repositórios e praticando Set

---
+++ curso_java/exercicios/src/personalChallengers/TirandoDoFor.java
@@ -0,0 +1,41 @@
+package personalChallengers;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TirandoDoFor {
+
+	public static void main(String[] args) {
+
+		Set <String> names = new TreeSet<>();
+		
+		names.add("Lucas");
+		names.add("Pedro");
+		names.add("João");
+		names.add("Paulo");
+		names.add("Ana");
+		
+		System.out.println(names);
+		
+		names.remove("Ana");
+		System.out.println(names);
+		
+		Set <String> lastnames = new HashSet<>();
+		
+		lastnames.add("Silva");
+		lastnames.add("Oliveira");
+		lastnames.add("Costa");
+		lastnames.add("Melo");
+		
+		System.out.println(lastnames);
+		
+		names.addAll(lastnames);
+		
+		System.out.println(names);
+		
+		names.removeAll(lastnames);
+		System.out.println(names);
+	}
+
+}
